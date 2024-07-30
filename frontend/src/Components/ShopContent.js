1e7f82f9d6ffadf9b2b5add61b2e6100991f7f91
Yuvraj gupta <136947539+YuvrajGuptaPrivate@users.noreply.github.com>
2024-07-30 11:40:09 +0530

    Update ShopContent.js

--- frontend/src/Components/ShopContent.js
+++ frontend/src/Components/ShopContent.js
@@ -31,7 +31,7 @@ function ShopContent() {
           {products.map(product => (
             <div key={product._id} className="col-12 col-md-4 col-lg-3 mb-5">
               <Link to={`/product/${product.id}`}> {/* Use Link for routing */}
-                <img alt="" src={product.image} className="img-fluid product-thumbnail" />
+                <img alt="" src={`/images/${product.image}`}  className="img-fluid product-thumbnail" />
                 <h3 className="product-title">{product.name}</h3>
                 <strong className="product-price">Rs{product.new_price}.00</strong>
               </Link>
