c7710f64e4e320a6e933bee6cb943bee1cc3077a
Yuvraj gupta <136947539+YuvrajGuptaPrivate@users.noreply.github.com>
2024-07-30 14:13:36 +0530

    Update Productsection.js

--- frontend/src/Components/Productsection.js
+++ frontend/src/Components/Productsection.js
@@ -26,10 +26,9 @@ function ProductSection() {
             <p className="mb-4">Discover our exquisite collection of custom curtains, blinds, and shades. From modern to traditional, we have the perfect solution for your home.</p>
             <p><a href="/Shop" className="btn">Explore</a></p>
           </div>
-
           <div className="col-md-9">
             <ul className="product-list row">
-              {newcollections.map((product, index) => (
+              {newCollections.map((product, index) => (
                 <li key={index} className="col-md-3 product-item">
                    <Link to={`/product/${product.id}`}> {/* Use Link for routing */}
                 <img alt="" src={product.image} className="img-fluid product-thumbnail" style={{ width: 261, height: 261 }} />
