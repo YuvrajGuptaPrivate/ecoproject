d3d7f56a0f8901a39c778b7fd8f4b69c344e9f27
Yuvraj gupta <136947539+YuvrajGuptaPrivate@users.noreply.github.com>
2024-11-29 15:26:38 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -40,7 +40,7 @@ const AddProduct = () => {
             {
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://ecoproject-backendd.onrender.com/addproduct',{
                 method: 'POST',
                 headers:{
                     Accept:'application/json',
